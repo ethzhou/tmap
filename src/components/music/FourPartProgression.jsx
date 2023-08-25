739c5a01e9443c563785adca8106f04eae52a6b1
ethzhou <ethzhou@gmail.com>
2023-08-25 00:30:00 -0700

    added measures; other fixes

--- src/components/music/FourPartProgression.jsx
+++ src/components/music/FourPartProgression.jsx
@@ -9,72 +9,147 @@ export default function FourPartProgression({
   chordsPerMeasure,
 }) {
   const divId = name ? `vf-${name}` : "vf-output";
-  // console.log(parts);
   useEffect(() => {
-    const div = document.getElementById(divId);
+    // Values
 
-    const renderer = new Renderer(div, Renderer.Backends.SVG);
-    renderer.resize(700, 300);
-    const context = renderer.getContext();
-    const formatter = new Formatter();
+    const staveX = 20;
+    const staveY = 60;
 
-    const trebleStave = new Stave(0, 0, 600).addClef("treble");
-    const bassStave = new Stave(0, 120, 600).addClef("bass");
-    trebleStave.addKeySignature(keySignature).addTimeSignature(timeSignature);
-    bassStave.addKeySignature(keySignature).addTimeSignature(timeSignature);
+    const [beatsPerMeasure, valuePerBeat] = timeSignature.split("/").map(item => Number(item));
+    const noteDuration = `${chordsPerMeasure * valuePerBeat / beatsPerMeasure}`;
+    console.log(`noteDuration ${noteDuration}`);
+
+    const chordCount = parts.soprano.length;
+    const measureCount = Math.ceil(chordCount / chordsPerMeasure);
+
+    const staveDistance = 115;
+    const measureWidth = chordsPerMeasure * 60;
+    
+    // Create symbols
+
+    const measureStaves = [];
+
+    const firstTrebleStave = new Stave(staveX, staveY, measureWidth);
+    const firstBassStave = new Stave(staveX, staveY + staveDistance, measureWidth);
+
+    firstTrebleStave.addClef("treble");
+    firstBassStave.addClef("bass");
+    firstTrebleStave.addKeySignature(keySignature).addTimeSignature(timeSignature);
+    firstBassStave.addKeySignature(keySignature).addTimeSignature(timeSignature);
 
     // Align the first notes of the staves
-    const startX = Math.max(trebleStave.getNoteStartX(), bassStave.getNoteStartX());
-    trebleStave.setNoteStartX(startX);
-    bassStave.setNoteStartX(startX);
+    // noteStartX is a value measuring from the border of the canvas
+    const noteStartX = Math.max(firstTrebleStave.getNoteStartX(), firstBassStave.getNoteStartX());
+    // relativeNoteStartX is a value measuring from the start of the stave
+    const relativeNoteStartX = noteStartX - staveX;
+    firstTrebleStave.setNoteStartX(noteStartX);
+    firstBassStave.setNoteStartX(noteStartX);
+    firstTrebleStave.setWidth(firstTrebleStave.getWidth() + relativeNoteStartX);
+    firstBassStave.setWidth(firstBassStave.getWidth() + relativeNoteStartX);
+    
+    measureStaves.push({ trebleStave: firstTrebleStave, bassStave: firstBassStave });
+    // Create more staves, one per measure, each starting where the last ended
+    for (let i = 1; i < measureCount; i++) {
+      const trebleStave = new Stave(
+        noteStartX + i * measureWidth,
+        staveY,
+        measureWidth
+      );
+      const bassStave = new Stave(
+        noteStartX + i * measureWidth,
+        staveY + staveDistance,
+        measureWidth
+      );
 
-    trebleStave.setContext(context).draw();
-    bassStave.setContext(context).draw();
-
-    const staveConnector = new StaveConnector(trebleStave, bassStave).setType("single");
-    staveConnector.setContext(context).draw();
-
-    const [beatsPerMeasure, valuePerBeat] = timeSignature.split("/").map(item => Number(item));  // beatsPerMeasure / valuePerBeat
-    const noteDuration = `${chordsPerMeasure * valuePerBeat / beatsPerMeasure}`;
-    console.log(`noteduration ${noteDuration}`);
-
-    const voices = { };
-    for (const voice in parts) {
-      const clef = (voice === "bass" || voice === "tenor") ? "bass" : "treble";
-      voices[voice] = new Voice();
-      voices[voice].setStave(clef === "bass" ? bassStave : trebleStave);
-      const staveNotes = parts[voice].map(pitch => new StaveNote({
-        clef: clef,
-        keys: [!pitch ? "B/3" : pitch.toVFKey()],
-        duration: noteDuration,
-        stem_direction: (voice === "bass" || voice === "alto") ? Stem.DOWN : Stem.UP,
-      }))
-      voices[voice].addTickables(staveNotes);
-      console.log("after", parts[voice], voices[voice]);
+      measureStaves.push({ trebleStave, bassStave });
     }
     
-    console.log("jjj", [Object.values(voices)]);
-    formatter.joinVoices([voices.bass, voices.tenor]);
-    formatter.joinVoices([voices.alto, voices.soprano]);
-    formatter.format(Object.values(voices), 600 - startX);
+    measureStaves.at(-1).trebleStave.setWidth(measureWidth + 14);
+    measureStaves.at(-1).bassStave.setWidth(measureWidth + 14);
+    const brace = new StaveConnector(
+      measureStaves[0].trebleStave,
+      measureStaves[0].bassStave,
+    ).setType("brace");
+    const doubleBarline = new StaveConnector(
+      measureStaves.at(-1).trebleStave,
+      measureStaves.at(-1).bassStave
+    ).setType("boldDoubleRight");
 
-    for (const voice in voices) {
-      voices[voice].draw(context);
-    }
+    const measureStavesAndConnectors = measureStaves.map(({ trebleStave, bassStave }) => {
+      const staveConnector = new StaveConnector(trebleStave, bassStave).setType("singleLeft");
 
-    // console.log(notes.map(note => note.toKey()));
+      return { trebleStave, bassStave, staveConnector };
+    })
 
-    // TODO accidentals
-    // for (let i = 0; i < notes.length; i++) {
-    //   if (notes[i].accidental) {
-    //     printedNotes[0].addModifier(new Accidental(accidentalString(notes[i].accidental)), i);
-    //   }
-    // }
-    // const voice = new Voice();
-    // voice.addTickables(printedNotes);
+    const music = measureStavesAndConnectors.map(({ trebleStave, bassStave, staveConnector }, i) => {
+      const voices = { };
+      for (const part in parts) {
+        const clef = (part === "bass" || part === "tenor") ? "bass" : "treble";
+        voices[part] = new Voice({ num_beats: beatsPerMeasure, beat_value: valuePerBeat });
+        voices[part].setStave(clef === "bass" ? bassStave : trebleStave);
+        const staveNotes = parts[part].slice(i * chordsPerMeasure, (i + 1) * chordsPerMeasure).map(
+          pitch => new StaveNote({
+            clef: clef,
+            keys: [!pitch ? "R/4" : pitch.toVFKey()],
+            duration: `${noteDuration}${!pitch ? "r" : ""}`,
+            stem_direction: (part === "bass" || part === "alto") ? Stem.DOWN : Stem.UP,
+          })
+        );
+        // console.log("after", parts[part], voices[part]);
+        voices[part].addTickables(staveNotes);
+      }
 
-    // voice.draw(context, trebleStave);
+      // TODO accidentals
+
+      return { trebleStave, bassStave, staveConnector, voices };
+    });
     
+    if (chordCount % chordsPerMeasure) {
+      const lastVoices = music.at(-1).voices;
+      for (const part in lastVoices) {
+        for (let i = 0; i < chordsPerMeasure - chordCount % chordsPerMeasure; i++) {
+          lastVoices[part].addTickable(new StaveNote({
+            keys: ["R/4"],
+            duration: `${noteDuration}r`,
+          }));
+        }
+      };
+    }
+      
+    console.log(music);
+
+    // Draw
+
+    const div = document.getElementById(divId);
+    const renderer = new Renderer(div, Renderer.Backends.SVG);
+
+    const rendererWidth = 40 + noteStartX + measureCount * measureWidth;
+    const rendererHeight = 360;
+    renderer.resize(rendererWidth, rendererHeight);
+    const context = renderer.getContext();
+    const formatter = new Formatter();
+    
+    music.forEach(({ trebleStave, bassStave, staveConnector, voices }, i) => {
+      trebleStave.setContext(context).draw();
+      bassStave.setContext(context).draw();
+
+      staveConnector.setContext(context).draw();
+
+      formatter.joinVoices([voices.bass, voices.tenor]);
+      formatter.joinVoices([voices.alto, voices.soprano]);
+      // formatter.alignRests(Object.values(voices));
+      formatter.format(
+        Object.values(voices),
+        measureWidth,
+        // { auto_beam: true, autobeam: true }
+      );
+
+      for (const voice in voices) {
+        voices[voice].draw(context);
+      }
+    })
+    brace.setContext(context).draw();
+    doubleBarline.setContext(context).draw();
   }, [parts, keySignature, timeSignature, chordsPerMeasure])
   
   return (
--- src/components/practice/exercises/FourPartNumerals.jsx
+++ src/components/practice/exercises/FourPartNumerals.jsx
@@ -3,7 +3,7 @@ import useExercise from "../../../hooks/useExercise";
 import FourPartProgression from "../../music/FourPartProgression";
 
 function f(s) {
-  return s.split(" ").map(e => Pitch.fromString(e));
+  return s.split(" ").map(e => e === "r" ? null : Pitch.fromString(e));
 }
 
 export default function FourPartNumerals() {
@@ -12,15 +12,15 @@ export default function FourPartNumerals() {
   );
 
   const parts = {
-    soprano: f("C5 D5 F5 A5"),
-    alto: f("E4 D4 C4 B3"),
-    tenor: f("G3 A3 F3 D3"),
-    bass: f("C2 F2 F2 G2"),
+    soprano: f("C5 D5 E6 r B5 C5 C5 D5 E6"),
+    alto: f("E4 F#3 F3 E4 G4 F4 E4 F#3 F3"),
+    tenor: f("G3 Ab4 F3 A4 E3 D3 G3 Ab4 F3"),
+    bass: f("C2 F2 F2 G1 E2 F2 C2 F2 F2"),
   };
 
   return (
     <>
-      <FourPartProgression parts={parts} keySignature="Cb" timeSignature="4/4" chordsPerMeasure={4} />
+      <FourPartProgression parts={parts} keySignature="Ab" timeSignature="2/4" chordsPerMeasure={2} />
       <div></div>
     </>
   )
