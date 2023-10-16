ee25092867b3d5e44d0d1fd6026236151f4dbb47
ethzhou <ethzhou@gmail.com>
2023-10-16 06:14:57 -0700

    light adjustments

--- src/components/practice/PracticePage.jsx
+++ src/components/practice/PracticePage.jsx
@@ -17,7 +17,7 @@ export default function PracticePage() {
   return (
     <>
       <div className="flex justify-center">
-        <div className="flex h-screen w-[40rem] flex-col justify-center gap-0.5">
+        <div className="mt-20 flex h-screen w-[48rem] flex-col justify-start gap-0.5">
           <nav className="flex items-baseline">
             <Link
               to=""
@@ -38,7 +38,7 @@ export default function PracticePage() {
           </nav>
           <div
             onMouseMove={handleMouseMoveLightPos}
-            className="group flex content-center items-start justify-center gap-8"
+            className="group flex flex-wrap items-start justify-between justify-items-center gap-y-6"
           >
             <div className="practice-card relative flex h-96 w-60 items-center justify-center bg-slate-500 text-center before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100">
               <div className="practice-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center bg-slate-100 dark:bg-slate-800">
@@ -111,9 +111,37 @@ export default function PracticePage() {
                       viewBox="0 0 122 150"
                       className="fill-slate-700 stroke-slate-700 dark:fill-slate-300 dark:stroke-slate-300"
                     >
-                      <use href="/src/assets/interval-dictation.svg#graphic"></use>
+                      <use href="/src/assets/four-part-harmony.svg#graphic"></use>
                     </svg>
                     <div className="font-clean">I - IV - V - I!</div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="practice-card relative flex h-96 w-60 items-center justify-center bg-slate-500 text-center before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100">
+              <div className="practice-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center bg-slate-100 dark:bg-slate-800">
+                <div className="h-full w-full px-4 text-center">
+                  <Link
+                    to=""
+                    className="flex h-full w-full flex-col items-center text-slate-600 no-underline dark:text-slate-300"
+                  >
+                    <div className="mb-4 mt-20 font-comic text-2xl">
+                      Something Else
+                    </div>
+                    <svg
+                      strokeWidth="0.3"
+                      xmlns="http://www.w3.org/2000/svg"
+                      strokeDasharray="none"
+                      width="122"
+                      height="150"
+                      viewBox="0 0 122 150"
+                      className="fill-slate-700 stroke-slate-700 dark:fill-slate-300 dark:stroke-slate-300"
+                    >
+                      <use href="/src/assets/something-else.svg#graphic"></use>
+                    </svg>
+                    <div className="font-clean">
+                      What's the lorem ipsum of music?
+                    </div>
                   </Link>
                 </div>
               </div>
