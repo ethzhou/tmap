f21ab5c576da2dad13edab06202a42a6cc1bf584
ethzhou <ethzhou@gmail.com>
2023-10-16 06:09:19 -0700

    removed border radius

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -41,10 +41,10 @@ export default function HomePage() {
           </nav>
           <nav
             onMouseMove={handleMouseMoveLightPos}
-            className="group relative flex h-80 w-full flex-wrap items-start justify-center justify-items-center gap-1 overflow-x-hidden"
+            className="group relative flex h-80 w-full flex-wrap items-start justify-center justify-items-center gap-1"
           >
-            <div className="home-card relative flex aspect-square flex-auto items-center justify-center rounded-sm bg-emerald-400 before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:rounded-sm before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:rounded-sm after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-emerald-600">
-              <div className="home-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center rounded-sm bg-emerald-600 dark:bg-teal-900">
+            <div className="home-card before after relative flex aspect-square flex-auto items-center justify-center bg-emerald-400 before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-emerald-600">
+              <div className="home-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center bg-emerald-600 dark:bg-teal-900">
                 <Link
                   to="learn"
                   className="flex h-full w-full items-center justify-center text-slate-50 no-underline dark:text-slate-200"
@@ -55,8 +55,8 @@ export default function HomePage() {
                 </Link>
               </div>
             </div>
-            <div className="home-card relative flex aspect-square flex-auto items-center justify-center rounded-sm bg-pink-400 before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:rounded-sm before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:rounded-sm after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-pink-600">
-              <div className="home-card-content absolute z-[2] h-[calc(100%-4px)] w-[calc(100%-4px)] rounded-sm bg-pink-600 dark:bg-pink-900">
+            <div className="home-card before after relative flex aspect-square flex-auto items-center justify-center bg-pink-400 before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-pink-600">
+              <div className="home-card-content absolute z-[2] h-[calc(100%-4px)] w-[calc(100%-4px)] bg-pink-600 dark:bg-pink-900">
                 <Link
                   to="practice"
                   className="flex h-full w-full items-center justify-center text-slate-50 no-underline dark:text-slate-200"
