0629f0ff084caa06fbf5a4de12fc880ea575c3de
ethzhou <ethzhou@gmail.com>
2023-10-17 23:24:15 -0700

    added blend modes for belt color for clarity

--- src/components/Belt.jsx
+++ src/components/Belt.jsx
@@ -3,7 +3,7 @@ import ColorSchemeIcon from "./ColorSchemeIcon";
 export default function Belt() {
   return (
     <>
-      <div className=" sticky top-4 z-[9999] flex gap-4">
+      <div className="sticky top-4 z-[9999] flex gap-4 mix-blend-multiply dark:mix-blend-screen">
         <div className="absolute right-4">
           <ColorSchemeIcon />
         </div>
