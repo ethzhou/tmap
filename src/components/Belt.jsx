b38934661fd537a93ae8991cc730e68775718d92
ethzhou <ethzhou@gmail.com>
2023-10-17 22:29:44 -0700

    raised belt z-index

--- src/components/Belt.jsx
+++ src/components/Belt.jsx
@@ -3,7 +3,7 @@ import ColorSchemeIcon from "./ColorSchemeIcon";
 export default function Belt() {
   return (
     <>
-      <div className="sticky top-4 flex gap-4">
+      <div className=" sticky top-4 z-[9999] flex gap-4">
         <div className="absolute right-4">
           <ColorSchemeIcon />
         </div>
