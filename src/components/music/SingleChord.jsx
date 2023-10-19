552d8a9220cc3b6b7ce767c9deb51b637330e0a6
ethzhou <ethzhou@gmail.com>
2023-10-19 01:08:58 -0700

    added scaling

--- src/components/music/SingleChord.jsx
+++ src/components/music/SingleChord.jsx
@@ -1,8 +1,15 @@
 import { useEffect } from "react";
-import { Accidental, Formatter, Renderer, Stave, StaveNote, Voice } from "vexflow";
+import {
+  Accidental,
+  Formatter,
+  Renderer,
+  Stave,
+  StaveNote,
+  Voice,
+} from "vexflow";
 import { accidentalToCode } from "../../utils/musicUtils";
 
-export default function SingleChord({ name, clef, pitches }) {
+export default function SingleChord({ name, clef, pitches, scaleFactor }) {
   const divId = name ? `vf-${name}` : "vf-canvas";
 
   useEffect(() => {
@@ -10,8 +17,9 @@ export default function SingleChord({ name, clef, pitches }) {
     const div = document.getElementById(divId);
 
     const renderer = new Renderer(div, Renderer.Backends.SVG);
-    renderer.resize(122, 150);
+    renderer.resize(122 * scaleFactor, 150 * scaleFactor);
     const context = renderer.getContext();
+    context.scale(scaleFactor, scaleFactor);
 
     const stave = new Stave(0, 0, 120, {
       space_above_staff_ln: 5,
@@ -20,17 +28,22 @@ export default function SingleChord({ name, clef, pitches }) {
     stave.setContext(context).draw();
 
     const printedNotes = [
-      new StaveNote({ keys: pitches.map(pitch => pitch.toVF()), duration: "w", clef: clef }),
+      new StaveNote({
+        keys: pitches.map(pitch => pitch.toVF()),
+        duration: "w",
+        clef: clef,
+      }),
     ];
     // Include accidentals where appropriate
     // #region Own accidental application method. Slightly different from VexFlow's Accidental.applyAccidentals: A unison with natural and sharp (like F F#) is rendered with no natural symbol via VexFlow Accidental.applyAccidentals, while this region's code would render a natural symbol. Something a unison with two sharp notes (like F# F#) would be rendered with two sharp symbols via VexFlow and one sharp symbol with this code. For some reason, in the case of flats, one flat note does render the natural symbol, but two flat notes still renders two flat symbols.
-    const accidentalRecord = { };  // { "C4": [-1, 0] }
+    const accidentalRecord = {}; // { "C4": [-1, 0] }
     for (let i = 0; i < pitches.length; i++) {
       if (!accidentalRecord[pitches[i].toSpace()])
         accidentalRecord[pitches[i].toSpace()] = [];
+
       accidentalRecord[pitches[i].toSpace()].push({
         index: i,
-        accidental: pitches[i].accidental
+        accidental: pitches[i].accidental,
       });
     }
     for (const basePitch in accidentalRecord) {
@@ -40,7 +53,7 @@ export default function SingleChord({ name, clef, pitches }) {
           if (item.accidental !== last) {
             printedNotes[0].addModifier(
               new Accidental(accidentalToCode(item.accidental)),
-              item.index
+              item.index,
             );
             last = item.accidental;
           }
@@ -48,6 +61,7 @@ export default function SingleChord({ name, clef, pitches }) {
       }
     }
     // #endregion
+
     const voice = new Voice();
     voice.addTickables(printedNotes);
     // console.log(voice);
@@ -55,9 +69,7 @@ export default function SingleChord({ name, clef, pitches }) {
     new Formatter().joinVoices([voice]).format([voice], 350);
 
     voice.draw(context, stave);
-  }, [clef, pitches])
-  
-  return (
-    <div key={pitches} id={divId}></div>
-  );
+  }, [clef, pitches]);
+
+  return <div key={pitches} id={divId}></div>;
 }
