c440acac0674cd6d394bdaf6e195c222d9e7948f
ethzhou <ethzhou@gmail.com>
2023-10-20 00:46:47 -0700

    decorated stopwatch

--- src/components/practice/exercises/IntervalExercise.jsx
+++ src/components/practice/exercises/IntervalExercise.jsx
@@ -29,11 +29,18 @@ export default function IntervalExercise({
             <div className="self-bottom h-[2px] flex-auto bg-pink-600"></div>
             <div className="font-mono text-2xl text-slate-700 no-underline dark:text-slate-200">
               {stopwatch.days !== 0 && <span>{stopwatch.days}:</span>}
-              {stopwatch.hours !== 0 && <span>{stopwatch.hours}:</span>}
-              {stopwatch.minutes !== 0 && (
+              {(stopwatch.days !== 0 || stopwatch.hours !== 0) && (
+                <span>{String(stopwatch.hours).padStart(2, "0")}:</span>
+              )}
+              {(stopwatch.days !== 0 ||
+                stopwatch.hours !== 0 ||
+                stopwatch.minutes !== 0) && (
                 <span>{String(stopwatch.minutes).padStart(2, "0")}:</span>
               )}
-              {stopwatch.seconds !== 0 && (
+              {(stopwatch.days !== 0 ||
+                stopwatch.hours !== 0 ||
+                stopwatch.minutes !== 0 ||
+                stopwatch.seconds !== 0) && (
                 <span>{String(stopwatch.seconds).padStart(2, "0")}</span>
               )}
             </div>
