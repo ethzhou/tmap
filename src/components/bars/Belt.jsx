14ba49002e146b851f286850668757beef5f3bc0
ethzhou <ethzhou@gmail.com>
2023-10-21 19:17:51 -0700

    fixed bar pointer events

--- src/components/bars/Belt.jsx
+++ src/components/bars/Belt.jsx
@@ -1,11 +1,13 @@
 export default function Belt() {
   return (
     <>
-      <div className="fixed bottom-0 z-[9999] w-full">
+      <div className="pointer-events-none fixed bottom-0 z-[9999] w-full">
         <div className="flex items-center gap-1 px-8 py-4 mix-blend-multiply dark:mix-blend-screen">
           <div className="flex-auto"></div>
-          <div className="font-hand text-xs text-slate-200 dark:text-slate-800">
-            ethzhou
+          <div className="flex gap-1 [&>*]:pointer-events-auto">
+            <div className="pointer-events-none select-none font-hand text-xs text-slate-200 dark:text-slate-800">
+              ethzhou
+            </div>
           </div>
         </div>
       </div>
--- src/components/bars/Necklace.jsx
+++ src/components/bars/Necklace.jsx
@@ -3,10 +3,10 @@ import ColorSchemeIcon from "./ColorSchemeIcon";
 export default function Necklace() {
   return (
     <>
-      <div className="fixed top-0 z-[9999] w-full">
+      <div className="pointer-events-none fixed top-0 z-[9999] w-full">
         <div className="flex items-end gap-1 px-8 py-4 mix-blend-multiply dark:mix-blend-screen">
           <div className="flex-auto"></div>
-          <div>
+          <div className="flex gap-1 [&>*]:pointer-events-auto">
             <ColorSchemeIcon />
           </div>
         </div>
