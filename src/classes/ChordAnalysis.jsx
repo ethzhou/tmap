10117202c5606984964071fb4ae766983c369708
ethzhou <ethzhou@gmail.com>
2023-10-21 18:49:39 -0700

    prettier

--- src/classes/ChordAnalysis.jsx
+++ src/classes/ChordAnalysis.jsx
@@ -11,7 +11,7 @@ export default class ChordAnalysis {
    * @property {string} arabic
    * @property {string} secondary
    */
-  constructor({accidental, roman, arabic, secondary}) {
+  constructor({ accidental, roman, arabic, secondary }) {
     this.accidental = accidental;
     this.roman = roman;
     this.arabic = arabic;
@@ -32,18 +32,21 @@ export default class ChordAnalysis {
 
   /**
    * Constructs an analysis from string notation. Returns `undefined` if invalid.
-   * 
+   *
    * @param {string} strRepresentation For example, "V65/IV" or "bii".
    * @returns {ChordAnalysis | undefined}
    */
   static fromString(strRepresentation) {
-    const groups = strRepresentation?.match(REGEXP_ChordAnalysis)?.groups || undefined;
+    const groups =
+      strRepresentation?.match(REGEXP_ChordAnalysis)?.groups || undefined;
 
     return groups ? new ChordAnalysis(groups) : undefined;
   }
 
   toString() {
-    return `${this.accidental ?? ""}${this.roman}${this.arabic ?? ""}${this.secondary ?? ""}`;
+    return `${this.accidental ?? ""}${this.roman}${this.arabic ?? ""}${
+      this.secondary ?? ""
+    }`;
   }
 
   isSeventh() {
@@ -51,28 +54,32 @@ export default class ChordAnalysis {
   }
 
   inversion() {
-    return ["42"].includes(this.arabic) ? 3
-    : ["43", "64"].includes(this.arabic) ? 2
-    : ["65", "6", "63"].includes(this.arabic) ? 1
-    : ["7", "53", "", undefined].includes(this.arabic) ? 0
-    // Otherwise, the arabic numerals are invalid
-    : -1;
+    return ["42"].includes(this.arabic)
+      ? 3
+      : ["43", "64"].includes(this.arabic)
+      ? 2
+      : ["65", "6", "63"].includes(this.arabic)
+      ? 1
+      : ["7", "53", "", undefined].includes(this.arabic)
+      ? 0
+      : // Otherwise, the arabic numerals are invalid
+        -1;
   }
 
   /**
    * Find the degree of the bass of the chord.
-   * 
+   *
    * @returns {number} Scale degree of the bass.
    */
   bassDegree() {
     const inversion = this.inversion();
 
-    return (this.degree + 2 * inversion - 1) % 7 + 1;
+    return ((this.degree + 2 * inversion - 1) % 7) + 1;
   }
 
   /**
    * Returns the pitch of the bass of the chord in a key.
-   * 
+   *
    * @param {Key} key
    * @returns {Pitch}
    */
@@ -81,12 +88,12 @@ export default class ChordAnalysis {
   }
 
   /**
-   * Finds the interval size between the roots of the analyses. 
-   * 
+   * Finds the interval size between the roots of the analyses.
+   *
    * @param {ChordAnalysis} other
    * @returns {number} The result is always positive.
    */
   movementTo(other) {
-    return (other.degree - this.degree + 7) % 7 + 1;
+    return ((other.degree - this.degree + 7) % 7) + 1;
   }
 }
--- src/classes/Pitch.jsx
+++ src/classes/Pitch.jsx
@@ -1,4 +1,9 @@
-import { A_OCTAVE, C_OCTAVE, accidentalToString, accidentalToText } from "../utils/musicUtils";
+import {
+  A_OCTAVE,
+  C_OCTAVE,
+  accidentalToString,
+  accidentalToText,
+} from "../utils/musicUtils";
 import Interval from "./Interval";
 
 export default class Pitch {
@@ -15,7 +20,7 @@ export default class Pitch {
 
   /**
    *  Constructs a pitch from an integer representing spacing away from middle C.
-   * 
+   *
    * @param {number} spacesFromC4 The number of spaces away from middle C.
    * @param {number} accidental An accidental to be tacked onto the note.
    * @returns {Pitch}
@@ -23,28 +28,36 @@ export default class Pitch {
   static fromInt(spacesFromC4, accidental = 0) {
     const octave = 4 + Math.floor(spacesFromC4 / 7);
     const letter = A_OCTAVE[(((2 + spacesFromC4) % 7) + 7) % 7];
-    
+
     return new Pitch(letter, accidental, octave);
   }
 
   /**
    * Constructs a pitch from string notation. Returns `undefined` if invalid.
-   * 
+   *
    * @param {string} strRepresentation For example, "C#4" or "D5".
    * @returns {Pitch | undefined}
    */
   static fromString(strRepresentation) {
     const letter = strRepresentation[0]?.toUpperCase();
-    const accidental = strRepresentation[1] === "b" ? (strRepresentation[2] === "b" ? -2 : -1)
-      : strRepresentation[1] === "#" ? (strRepresentation[2] === "#" ? 2 : 1)
-      : strRepresentation[1] === "x" ? 2
-      : 0;
+    const accidental =
+      strRepresentation[1] === "b"
+        ? strRepresentation[2] === "b"
+          ? -2
+          : -1
+        : strRepresentation[1] === "#"
+        ? strRepresentation[2] === "#"
+          ? 2
+          : 1
+        : strRepresentation[1] === "x"
+        ? 2
+        : 0;
     const octave = Number(strRepresentation.slice(1 + Math.abs(accidental)));
 
     // Check validity
-    if (!C_OCTAVE.includes(letter))
-      return;
-    if (Number.isNaN(octave))  // octave is NaN: either the accidental was invalid or octave was invalid
+    if (!C_OCTAVE.includes(letter)) return;
+    if (Number.isNaN(octave))
+      // octave is NaN: either the accidental was invalid or octave was invalid
       return;
 
     return new Pitch(letter, accidental, octave);
@@ -52,15 +65,20 @@ export default class Pitch {
 
   /**
    * Constructs a pitch from a pitch and interval.
-   * 
+   *
    * @param {Pitch} pitch From this pitch.
    * @param {Interval} interval
    * @returns {Pitch}
    */
   static fromInterval(pitch, interval) {
     const newPitch = pitch.scaleTone(interval.size);
-    newPitch.accidental += Interval.qualityToAccidental(interval.quality, interval.size);
-    newPitch.octave += Math.floor((interval.size - 1) / 7) + pitch.letterIsAfterInOctave(newPitch.letter);
+    newPitch.accidental += Interval.qualityToAccidental(
+      interval.quality,
+      interval.size,
+    );
+    newPitch.octave +=
+      Math.floor((interval.size - 1) / 7) +
+      pitch.letterIsAfterInOctave(newPitch.letter);
 
     return newPitch;
   }
@@ -69,19 +87,23 @@ export default class Pitch {
    * @returns {string}
    */
   toString() {
-    return `${this.letter}${accidentalToString(this.accidental) ?? ""}${this.octave}`;
+    return `${this.letter}${accidentalToString(this.accidental) ?? ""}${
+      this.octave
+    }`;
   }
-  
+
   /**
    * @returns {string}
    */
   toText() {
-    return `${this.letter}${accidentalToText(this.accidental) ?? ""}${this.octave}`;
+    return `${this.letter}${accidentalToText(this.accidental) ?? ""}${
+      this.octave
+    }`;
   }
 
   /**
    * Returns the letter and octave of the pitch as a string.
-   * 
+   *
    * @returns {string}
    */
   toSpace() {
@@ -90,7 +112,7 @@ export default class Pitch {
 
   /**
    * Returns the letter and accidental of the pitch as a string.
-   * 
+   *
    * @returns {string}
    */
   toName() {
@@ -99,7 +121,9 @@ export default class Pitch {
 
   // 'key' by the terminology of VexFlow, as in `new StaveNote({keys: ['C/4', ...], ...})`
   toVF() {
-    return `${this.letter}${accidentalToString(this.accidental)}/${this.octave}`;
+    return `${this.letter}${accidentalToString(this.accidental)}/${
+      this.octave
+    }`;
   }
 
   // EasyScore notation
@@ -107,29 +131,23 @@ export default class Pitch {
     return `${this.letter}${accidentalToString(this.accidental)}${this.octave}`;
   }
 
-  alter(steps) {
+  alter(steps) {}
 
-  }
+  raise(steps) {}
 
-  raise(steps) {
-    
-  }
-
-  lower(steps) {
-
-  }
+  lower(steps) {}
 
   static halfstepsToNextLetter(letter) {
     return "BE".includes(letter) ? 1 : 2;
   }
-  
+
   static halfstepsToPrevLetter(letter) {
     return "CF".includes(letter) ? 1 : 2;
   }
 
   /**
    * Counts both the number of halfsteps to reach another letter and the simple interval size from pitch letter only.
-   * 
+   *
    * @param {Pitch} other
    * @returns {{ halfsteps: number, spaces: number }} Neither count includes the starting pitch.
    */
@@ -143,42 +161,40 @@ export default class Pitch {
       spaces += 1;
       i = (i + 1) % 7;
     }
-    spaces += (other.octave - this.octave - this.letterIsAfterInOctave(other)) * 7;
-    
+    spaces +=
+      (other.octave - this.octave - this.letterIsAfterInOctave(other)) * 7;
+
     return { halfsteps: halfsteps, spaces: spaces };
   }
 
   /**
    * @param {string | Pitch} other
-   * @returns {boolean} 
+   * @returns {boolean}
    */
   static prevLetterInOctave(other) {
-    if (typeof other === "object") 
-      other = other.letter;
+    if (typeof other === "object") other = other.letter;
     // Modify via ASCII, but check for end of loop
     return other === "A" ? "G" : String.fromCharCode(other.charCodeAt(0) - 1);
   }
 
   /**
    * @param {string | Pitch} letter
-   * @returns {boolean} 
+   * @returns {boolean}
    */
   static nextLetterInOctave(letter) {
-    if (typeof letter === "object") 
-      other = other.letter;
+    if (typeof letter === "object") other = other.letter;
     // Modify via ASCII, but check for end of loop
     return letter === "G" ? "A" : String.fromCharCode(letter.charCodeAt(0) + 1);
   }
 
   /**
    * Finds whether this pitch's letter falls before another's in the octave from C.
-   * 
+   *
    * @param {string | Pitch} other
    * @returns {boolean}
    */
   letterIsBeforeInOctave(other) {
-    if (typeof other === "object")
-      other = other.letter;
+    if (typeof other === "object") other = other.letter;
     const thisIndex = C_OCTAVE.indexOf(this.letter);
     const otherIndex = C_OCTAVE.indexOf(other);
     console.assert(thisIndex > -1 && otherIndex > -1);
@@ -188,13 +204,12 @@ export default class Pitch {
 
   /**
    * Finds whether this pitch's letter falls after another's in the octave from C.
-   * 
+   *
    * @param {string | Pitch} other
    * @returns {boolean}
    */
   letterIsAfterInOctave(other) {
-    if (typeof other === "object")
-      other = other.letter;
+    if (typeof other === "object") other = other.letter;
     const thisIndex = C_OCTAVE.indexOf(this.letter);
     const otherIndex = C_OCTAVE.indexOf(other);
     console.assert(thisIndex > -1 && otherIndex > -1);
@@ -204,74 +219,74 @@ export default class Pitch {
 
   /**
    * Finds whether this pitch and another share the same letter and octave.
-   * 
+   *
    * @param {Pitch} other
    * @returns {boolean | undefined}
    */
   isSameSpaceAs(other) {
-    if (!other)
-      return;
+    if (!other) return;
     return this.letter === other.letter && this.octave === other.octave;
   }
 
   /**
    * Finds whether this pitch and another share the same letter and accidental.
-   * 
+   *
    * @param {Pitch} other
    * @returns {boolean | undefined}
    */
   isSameNameAs(other) {
-    if (!other)
-      return;
+    if (!other) return;
     return this.letter === other.letter && this.accidental === other.accidental;
   }
 
   /**
    * Finds whether this pitch and another are enharmonic.
-   * 
+   *
    * @param {Pitch} other
    * @returns {boolean}
    */
   isEnharmonicTo(other) {
     if (!other) return;
-    return this.#countTo(other).halfsteps === this.accidental - other.accidental;
+    return (
+      this.#countTo(other).halfsteps === this.accidental - other.accidental
+    );
   }
-  
+
   /**
    * Finds whether this pitch is lower than another.
-   * 
+   *
    * @param {Pitch} other
    * @param {boolean} considerAccidentals Whether or not to keep accidentals in the determination.
    * @returns {boolean}
    */
   isLowerThan(other, considerAccidentals = true) {
-    if (considerAccidentals)
-      return this.halfstepsTo(other) > 0;
+    if (considerAccidentals) return this.halfstepsTo(other) > 0;
     else
-      return this.octave < other.octave
-        || (this.octave === other.octave
-          && this.letterIsBeforeInOctave(other));
+      return (
+        this.octave < other.octave ||
+        (this.octave === other.octave && this.letterIsBeforeInOctave(other))
+      );
   }
-  
+
   /**
    * Finds whether this pitch is higher than another.
-   * 
+   *
    * @param {Pitch} other
    * @param {boolean} considerAccidentals Whether or not to keep accidentals in the determination.
    * @returns {boolean}
    */
   isHigherThan(other, considerAccidentals = true) {
-    if (considerAccidentals)
-      return this.halfstepsTo(other) < 0;
+    if (considerAccidentals) return this.halfstepsTo(other) < 0;
     else
-      return this.octave > other.octave
-        || (this.octave === other.octave
-          && this.letterIsAfterInOctave(other));
+      return (
+        this.octave > other.octave ||
+        (this.octave === other.octave && this.letterIsAfterInOctave(other))
+      );
   }
 
   /**
    * Counts the number of halfsteps up to reach another pitch. The start pitch is excluded from the count.
-   * 
+   *
    * @param {Pitch} other The end pitch.
    * @returns {number} Number of halfsteps up, or down if negative.
    */
@@ -280,16 +295,17 @@ export default class Pitch {
       return -other.halfstepsTo(this);
     }
 
-    const halfsteps = this.#countTo(other).halfsteps
-      + (other.octave - this.octave - this.letterIsAfterInOctave(other)) * 11
-      + (other.accidental - this.accidental);
+    const halfsteps =
+      this.#countTo(other).halfsteps +
+      (other.octave - this.octave - this.letterIsAfterInOctave(other)) * 11 +
+      (other.accidental - this.accidental);
 
     return halfsteps;
   }
 
   /**
    * Counts the number of spaces (and lines) up to reach another pitch. The start pitch is excluded from the count.
-   * 
+   *
    * @param {Pitch} other The end pitch.
    * @returns {number} Number of spaces up, or down if negative.
    */
@@ -304,8 +320,8 @@ export default class Pitch {
   }
 
   /**
-   * Finds the requested scale tone of the major scale of the current pitch. 
-   * 
+   * Finds the requested scale tone of the major scale of the current pitch.
+   *
    * @param {number} n Degree of the scale.
    * @returns {Pitch}
    */
@@ -323,28 +339,34 @@ export default class Pitch {
     // Two notes per complete octave are only a halfstep away
     newAccidental -= 2 * Math.floor((n - 1) / 7);
     // In the last octave, the fourth note of each tetrachord is only a halfstep away
-    newAccidental -= Math.floor(((n - 1) % 7 + 1) / 4);
-    const newOctave = this.octave + Math.floor((n - 1) / 7) + this.letterIsAfterInOctave(newLetter);
+    newAccidental -= Math.floor((((n - 1) % 7) + 1) / 4);
+    const newOctave =
+      this.octave +
+      Math.floor((n - 1) / 7) +
+      this.letterIsAfterInOctave(newLetter);
 
     return new Pitch(newLetter, newAccidental, newOctave);
   }
 
   /**
    * Finds the interval between two pitches.
-   * 
-   * @param {Pitch} other 
+   *
+   * @param {Pitch} other
    * @returns {Interval}
    */
   interval(other) {
     if (this.isHigherThan(other)) {
       return other.interval(this);
     }
-    
+
     const intervalSize = this.#countTo(other).spaces + 1;
 
     const baseScaleTone = this.scaleTone(intervalSize);
     const accidental = other.accidental - baseScaleTone.accidental;
-    const intervalQuality = Interval.accidentalToQuality(accidental, intervalSize);
+    const intervalQuality = Interval.accidentalToQuality(
+      accidental,
+      intervalSize,
+    );
 
     return new Interval(intervalQuality, intervalSize);
   }
