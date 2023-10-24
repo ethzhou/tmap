b24793d5fe3ece105b7850577ec52b9cd011d25d
ethzhou <ethzhou@gmail.com>
2023-10-23 21:16:10 -0700

    fixed octave halfstep count

--- src/classes/Interval.jsx
+++ src/classes/Interval.jsx
@@ -138,7 +138,9 @@ export default class Interval {
    */
   halfsteps() {
     const halfsteps =
-      MAJOR_SIMPLE_INTERVAL_HALFSTEP_COUNTS[this.simple - 1] +
+      (this.size === 8
+        ? 1
+        : MAJOR_SIMPLE_INTERVAL_HALFSTEP_COUNTS[this.simple - 1]) +
       Math.floor((this.size - 1) / 7) * 11 +
       Interval.qualityToAccidental(this.quality, this.size);
 
