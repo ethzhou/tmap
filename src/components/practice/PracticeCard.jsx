fe86f828fc38f0a733ce3ad90ae11feb2c12c004
ethzhou <ethzhou@gmail.com>
2023-11-12 22:38:56 -0800

    practice page cards, graphics

---
+++ src/components/practice/PracticeCard.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+export default function PracticeCard({
+  name,
+  blurb,
+  to,
+  svgViewBox,
+  graphicHRef,
+}) {
+  return (
+    <>
+      <div className="practice-card relative flex h-96 items-center justify-center bg-slate-400 text-center before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-slate-400">
+        <div className="practice-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center bg-slate-100 dark:bg-slate-800">
+          <div className="h-full w-full text-center">
+            <Link
+              to={to}
+              className="flex h-full w-full flex-col items-center text-slate-600 no-underline dark:text-slate-300"
+            >
+              <div className="flex h-full w-[94%] flex-col items-center">
+                <div className="mb-6 mt-20 px-4 font-display text-xl">
+                  {name}
+                </div>
+                {/* This div's height and width are independent of the SVG's */}
+                <div className="flex h-[100px] w-[150px] content-center justify-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    // svgViewBox is likely "0 0 W H", where W and H are the width and height of the source graphic
+                    viewBox={svgViewBox}
+                    className="fill-slate-600 stroke-slate-600 dark:fill-slate-300 dark:stroke-slate-300"
+                  >
+                    <use
+                      href={new URL(graphicHRef, import.meta.url).href}
+                    ></use>
+                  </svg>
+                </div>
+                <div className="mt-10 font-text">{blurb}</div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/practice/PracticePage.jsx
+++ src/components/practice/PracticePage.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import PracticeCard from "./PracticeCard";
 
 export default function PracticePage() {
   function handleMouseMoveLightPos(event) {
@@ -44,124 +45,40 @@ export default function PracticePage() {
             onMouseMove={handleMouseMoveLightPos}
             className="group grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(15rem,1fr))] gap-1"
           >
-            <div className="practice-card relative flex h-96 items-center justify-center bg-slate-400 text-center before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-slate-400">
-              <div className="practice-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center bg-slate-100 dark:bg-slate-800">
-                <div className="h-full w-full text-center">
-                  <Link
-                    to="interval-reading"
-                    className="flex h-full w-full flex-col items-center text-slate-600 no-underline dark:text-slate-300"
-                  >
-                    <div className="flex h-full w-full flex-col items-center px-4">
-                      <div className="mb-4 mt-20 px-4 font-display text-xl">
-                        Interval Reading
-                      </div>
-                      <svg
-                        strokeWidth="0.3"
-                        xmlns="http://www.w3.org/2000/svg"
-                        strokeDasharray="none"
-                        width="122"
-                        height="150"
-                        viewBox="0 0 122 150"
-                        className="fill-slate-600 stroke-slate-600 dark:fill-slate-300 dark:stroke-slate-300"
-                      >
-                        <use href="/src/assets/interval-reading.svg#graphic"></use>
-                      </svg>
-                      <div className="font-text">Okay.</div>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="practice-card relative flex h-96 items-center justify-center bg-slate-400 text-center before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-slate-400">
-              <div className="practice-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center bg-slate-100 dark:bg-slate-800">
-                <div className="h-full w-full text-center">
-                  <Link
-                    to="interval-dictation"
-                    className="flex h-full w-full flex-col items-center text-slate-600 no-underline dark:text-slate-300"
-                  >
-                    <div className="flex h-full w-full flex-col items-center px-4">
-                      <div className="mb-4 mt-20 px-4 font-display text-xl">
-                        Interval Dictation
-                      </div>
-                      <svg
-                        strokeWidth="0.3"
-                        xmlns="http://www.w3.org/2000/svg"
-                        strokeDasharray="none"
-                        width="122"
-                        height="150"
-                        viewBox="0 0 122 150"
-                        className="fill-slate-600 stroke-slate-600 dark:fill-slate-300 dark:stroke-slate-300"
-                      >
-                        <use href="/src/assets/interval-dictation.svg#graphic"></use>
-                      </svg>
-                      <div className="font-text">hm–⁠hm… hm… hm…</div>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="practice-card relative flex h-96 items-center justify-center bg-slate-400 text-center before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-slate-400">
-              <div className="practice-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center bg-slate-100 dark:bg-slate-800">
-                <div className="h-full w-full text-center">
-                  <Link
-                    to="four-part-harmony"
-                    className="flex h-full w-full flex-col items-center text-slate-600 no-underline dark:text-slate-300"
-                  >
-                    <div className="flex h-full w-full flex-col items-center px-4">
-                      <div className="mb-4 mt-20 px-4 font-display text-xl">
-                        Four-Part Harmony
-                      </div>
-                      <svg
-                        strokeWidth="0.3"
-                        xmlns="http://www.w3.org/2000/svg"
-                        strokeDasharray="none"
-                        width="122"
-                        height="150"
-                        viewBox="0 0 122 150"
-                        className="fill-slate-600 stroke-slate-600 dark:fill-slate-300 dark:stroke-slate-300"
-                      >
-                        <use href="/src/assets/four-part-harmony.svg#graphic"></use>
-                      </svg>
-                      <div className="font-text">
-                        IV - V - I<br />
-                        IV - V - I<br />
-                        IV - V - I
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="practice-card relative flex h-96 items-center justify-center bg-slate-400 text-center before:pointer-events-none before:absolute before:inset-0 before:z-[3] before:h-full before:w-full before:opacity-0 before:transition-opacity before:duration-500 before:content-[''] after:absolute after:inset-0 after:z-[1] after:h-full after:w-full after:opacity-0 after:transition-opacity after:duration-500 after:content-[''] hover:before:opacity-100 group-hover:after:opacity-100 dark:bg-slate-400">
-              <div className="practice-card-content absolute z-[2] flex h-[calc(100%-4px)] w-[calc(100%-4px)] items-center justify-center bg-slate-100 dark:bg-slate-800">
-                <div className="h-full w-full text-center">
-                  <Link
-                    to=""
-                    className="flex h-full w-full flex-col items-center text-slate-600 no-underline dark:text-slate-300"
-                  >
-                    <div className="flex h-full w-full flex-col items-center px-4">
-                      <div className="mb-4 mt-20 font-display text-xl">
-                        Something Else
-                      </div>
-                      <svg
-                        strokeWidth="0.3"
-                        xmlns="http://www.w3.org/2000/svg"
-                        strokeDasharray="none"
-                        width="122"
-                        height="150"
-                        viewBox="0 0 122 150"
-                        className="fill-slate-600 stroke-slate-600 dark:fill-slate-300 dark:stroke-slate-300"
-                      >
-                        <use href="/src/assets/something-else.svg#graphic"></use>
-                      </svg>
-                      <div className="font-text">
-                        what's the lorem ipsum of music
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-            </div>
+            <PracticeCard
+              name={"Interval Reading"}
+              blurb={"Okay."}
+              to={"interval-reading"}
+              svgViewBox={"0 0 160 120"}
+              graphicHRef={"/src/assets/interval-reading.svg#graphic"}
+            />
+            <PracticeCard
+              name={"Interval Dictation"}
+              blurb={"hm–⁠hm… hm… hm…"}
+              to={"interval-dictation"}
+              svgViewBox={"0 0 160 120"}
+              graphicHRef={"/src/assets/interval-dictation.svg#graphic"}
+            />
+            <PracticeCard
+              name={"Four-Part Harmony"}
+              blurb={
+                <>
+                  IV - V - I<br />
+                  IV - V - I<br />
+                  IV - V - I
+                </>
+              }
+              to={"four-part-harmony"}
+              svgViewBox={"0 0 160 120"}
+              graphicHRef={"/src/assets/four-part-harmony.svg#graphic"}
+            />
+            <PracticeCard
+              name={"Something Else"}
+              blurb={"what's the lorem ipsum of music"}
+              to={""}
+              svgViewBox={"0 0 160 120"}
+              graphicHRef={"/src/assets/something-else.svg#graphic"}
+            />
           </div>
         </div>
       </div>
