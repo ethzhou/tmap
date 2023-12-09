04c275c5fbd7b0a383fb7181a50d9c87d9ec6403
ethzhou <ethzhou@gmail.com>
2023-12-09 01:09:33 -0800

    NavTo adjustments

--- src/components/general/PracticeInvitation.jsx
+++ src/components/general/PracticeInvitation.jsx
@@ -2,7 +2,7 @@ import NavTo from "./NavTo";
 
 export default function PracticeInvitation({ to }) {
   return (
-    <span className="font-comic text-xl text-pink-500 dark:text-fuchsia-500">
+    <span className="font-straight text-xl text-pink-500 dark:text-pink-800">
       <NavTo to={to}>Practice!</NavTo>
     </span>
   );
